2cae6eb5769c1ab63db263e14b0c9256c16534f3
johan <johanvargas@seven4n.com>
2021-03-07 23:29:40 -0500

    añadidas distintas funciones

--- src/main/scala/co/s4n/listex/ListEx.scala
+++ src/main/scala/co/s4n/listex/ListEx.scala
@@ -1,30 +1,82 @@
 package co.s4n.listex
 
+import scala.util.Random
+
 
 object ListEx extends App {
 
+  /**
+   * Genera los subconjuntos de una lista.
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def subs[A](lst: List[A]): List[List[A]] = lst match {
     case Nil => List(Nil)
     case head :: tail => subs(tail).map(head :: _) ::: subs(tail)
   }
 
+  /**
+   * retorna una lista con las combinaciones de un elemento frente a los demás elementos
+   *
+   * @param a
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def barajar[A](a: A, lst: List[A]): List[List[A]] = lst match {
     case Nil => List(List(a))
     case head :: tail => (a :: (head :: tail)) :: ((barajar(a, tail).map(head :: _)))
   }
 
+  /**
+   * Genera todas las combinaciones de los elementos de una lista
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def permute[A](lst: List[A]): List[List[A]] = lst match {
     case Nil => List(Nil)
     case head :: tail => (permute(tail)).flatMap(barajar(head, _))
 
   }
 
+  /**
+   * Retorna el penultimo y el último objeto de una lista
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  def penultimoYUltimo[A](lst: List[A]): List[A] = lst match {
+    case Nil => Nil
+    //case head :: Nil =>
+    case head :: tail :: Nil => List(head, tail)
+    case head :: tail => penultimoYUltimo(tail)
+  }
+
+  /**
+   * Ejercicio 1 myLast Retorna el último objeto de una lista
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def myLast[A](lst: List[A]): Option[A] = lst match {
     case Nil => None
     case head :: Nil => Some(head)
     case head :: tail => myLast(tail)
   }
 
+  /**
+   * Ejercicio 2 myButLast Retorna el penúltimo objeto de una lista
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def myButLast[A](lst: List[A]): Option[A] = lst match {
     case Nil => None
     case head :: Nil => None
@@ -32,56 +84,233 @@ object ListEx extends App {
     case head :: tail => myButLast(tail)
   }
 
-  def penultimoYUltimo[A](lst: List[A]): List[A] = lst match {
-    //case Nil => None
-    //case head :: Nil =>
-    case head :: tail :: Nil => List(head, tail)
-    case head :: tail => penultimoYUltimo(tail)
-  }
 
+  /**
+   * Ejercicio 3 meno Retorna un elemento pedido en una posición dada.
+   *
+   * @param lst
+   * @param n
+   * @tparam A
+   * @return
+   */
   def elementAt[A](lst: List[A], n: Int): A = (n, lst) match {
+    case (0, head :: tail) => head
     case (1, head :: tail) => head
     case (n, head :: tail) => elementAt(tail, n - 1)
   }
 
+  /**
+   * Ejercicio 4 Retorna el tamaño de una lista
+   *
+   * @param list
+   * @tparam A
+   * @return
+   */
   def mylenght[A](list: List[A]): Int = list match {
     case Nil => 0
     case head :: tail => 1 + mylenght(tail)
   }
 
+  /**
+   * Retorna el tamaño de una lista usando foldRight
+   *
+   * @param list
+   * @tparam A
+   * @return
+   */
   def mylenghtR[A](list: List[A]): Int = list.foldRight(0)((_, y) => 1 + y)
 
-
+  /**
+   * Retorna el tamaño de una usando foldLeft
+   *
+   * @param list
+   * @tparam A
+   * @return
+   */
   def mylenghtL[A](list: List[A]): Int = list.foldLeft(0)((y, _) => y + 1)
 
+  /**
+   * Ejercicio 5 myReverse
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  def myReverse[A](lst: List[A]): List[A] = lst match {
+    case head :: tail => myReverse(tail) ::: List(head)
+    case Nil => Nil
+  }
 
+  /**
+   * Ejercicio 6 Retorna true si los elementos de una lista son palíndromos
+   *
+   * @param list
+   * @tparam A
+   * @return
+   */
   def isPalindrome[A](lst: List[A]): Boolean = lst == (lst).reverse
 
-
-
+  /*-------------------------------------------Trait nested list--------------------------------------------------------*/
   sealed trait NList[+A]
 
   case class Elem[A](elem: A) extends NList[A]
 
   case class Const[A](lst: List[NList[A]]) extends NList[A]
 
-  val y = Const(List(Elem(2), Const(List(Elem(1), Const(List(Elem(3), Elem(4))), Elem(5)))))
-
+  /**
+   * Ejercicio 7 aplana un arbol de listas en una lista simple.
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def flatten[A](lst: NList[A]): List[A] = lst match {
     case Elem(value) => List(value)
     case Const(list) => list.flatMap(flatten)
   }
 
+  /**
+   * Ejercicio 8 Elimina elementos repetidos de una lista
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
   def compress[A](lst: List[A]): List[A] = lst match {
     case ::(head, next) => head :: compress(next.dropWhile(head == _))
     case Nil => Nil
   }
 
-/* def pack[A](lst: List[A]): List[A] = lst match {
-    case ::(head, next) =>
-    case Nil =>
+  /**
+   * Ejercicio 9 separa elementos repetidos por listas distintas
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  def pack[A](lst: List[A]): List[List[A]] = lst match {
+    case Nil => Nil
+    case x :: tail => val (head, tail) = lst.span(x == _); head :: pack(tail)
   }
-*/
+
+  /**
+   * Ejercicio 10 genera una lista con tuplas que se componen de la cantidad de objetos de una lista y el objeto
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  def encode[A](lst: List[A]): List[(Int, A)] = pack(lst) match {
+    case Nil => Nil
+    case lst => lst.map(lista => (lista.length, lista.head))
+  }
+
+  /**
+   * Ejercicio 11 genera una lista con tuplas que se componen de la cantidad de objetos de una lista de elementos duplicados
+   * y retornando un único objeto cuando no tenga elementos duplicados
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  def encodeModified[A](lst: List[A]): List[_] = pack(lst) match {
+    case Nil => Nil
+    case lst => lst.map(lista => {
+      if (lista.length > 1) (lista.length, lista.head)
+      else lista.head
+    })
+  }
+
+  /**
+   * Ejercicio 12 retorna una lista resultado del proceso inverso a encode
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  /*  def decodeModified[A](lst: List[(A,A)]): Any = lst match {
+      case Nil =>
+      case (head:: tail) => println(head._1); println(head._2); decodeModified(tail)
+    }*/
+
+  def decodeModified[A](lst: List[A]): Any = lst match {
+    case Nil => Nil
+    case List() => print(" ")
+    case List((n, m)) =>  println(m); decodeModified(lst.tail)
+    case lst => println(lst.head); decodeModified(lst.tail)
+  }
 
 
+  /**
+   * 22
+   *
+   * @param from
+   * @param to
+   * @tparam A
+   * @return
+   */
+  def range[A](from: Int, to: Int): List[Int] = {
+    def aux[A](from: Int, to: Int, lstAcc: List[Int]): List[Int] = (from compare to) match {
+      case 0 => lstAcc ::: List(to)
+      case -1 => aux(from + 1, to, lstAcc ::: List(from))
+
+    }
+
+    aux(from, to, Nil)
+  }
+
+  /**
+   * 23
+   *
+   * @param lst
+   * @param n
+   * @tparam A
+   * @return
+   */
+  def myrndSelect[A](lst: List[A], n: Int): List[A] = {
+    def aux(lst: List[A], lstAcc: List[A], n: Int): List[A] = n match {
+      case 0 => lstAcc
+      case n => aux(lst, (elementAt(lst, Random.nextInt(lst.length)) :: lstAcc), n - 1)
+    }
+
+    aux(lst, Nil, n)
+  }
+
+  /**
+   * 24
+   *
+   * @param n
+   * @param max
+   * @tparam A
+   * @return
+   */
+  def mydiffSelect[A](n: Int, max: Int): List[Int] = myrndSelect(range(1, max), n)
+
+  /**
+   * 25
+   *
+   * @param lst
+   * @tparam A
+   * @return
+   */
+  def myRndmPermute[A](lst: List[A]): List[A] = myrndSelect(permute(lst), 1).flatten
+
+  def myRndmPermute2[A](lst: List[A]): List[A] = {
+    val permutada = permute(lst)
+    elementAt(permutada, Random.nextInt(permutada.length))
+  }
+
+  /**
+   * 26
+   *
+   * @param lst
+   * @param k
+   * @tparam A
+   * @return
+   */
+  def mycombinations[A](lst: List[A], k: Int): List[List[A]] = lst match {
+    case head :: tail if k == 1 => lst.map(List(_))
+    case head :: tail => (mycombinations(tail, k - 1).flatMap(barajar(head, _)))
+    case Nil => List(Nil)
+  }
 }
--- src/test/scala/co/s4n/listex/ListExSpec.scala
+++ src/test/scala/co/s4n/listex/ListExSpec.scala
@@ -4,7 +4,321 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class ListExSpec extends AnyFlatSpec with Matchers {
-  /* "The Hello object" should "say hello" in {
-     Hello.greeting shouldEqual "hello"
-   }*/
+
+  "1. subs(List('A','B','C')" should "List(List(), List('A'), List('B'), List('C'), List('A', 'B'), List('A','C'), " +
+    "List('B', 'C'), List('A', 'B', 'C'))" in {
+    val list = List("A", "B", "C")
+    val result = List(List(), List("A"), List("B"), List("C"), List("A", "B"), List("A", "C"), List("B", "C"), List("A", "B", "C"))
+    ListEx.subs(list).sortBy(_.length) shouldEqual result
+  }
+
+  "2. subs(List('A','B')" should "List(List(), List('A'), List('B'), List('A', 'B'))" in {
+    val list = List("A", "B")
+    val result = List(List(), List("A"), List("B"), List("A", "B"))
+    ListEx.subs(list).sortBy(_.length) shouldEqual result
+  }
+
+  "3. subs(List(1,2)" should "List(List(), List(1), List(2), List(1, 2))" in {
+    val list = List(1, 2)
+    val result = List(List(), List(1), List(2), List(1, 2))
+    ListEx.subs(list).sortBy(_.length) shouldEqual result
+  }
+
+
+  "4. permute(List(1,2))" should "List(List(1, 2), List(2, 1))" in {
+    val list = List(1, 2)
+    val result = List(List(1, 2), List(2, 1))
+    ListEx.permute(list) shouldEqual result
+  }
+
+  "5. permute(List(1,2,3))" should "List(List(1, 2, 3), List(2, 1, 3), List(2, 3, 1), List(1, 3, 2), List(3, 1, 2), List(3, 2, 1))" in {
+    val list = List(1, 2, 3)
+    val result = List(List(1, 2, 3), List(2, 1, 3), List(2, 3, 1), List(1, 3, 2), List(3, 1, 2), List(3, 2, 1))
+    ListEx.permute(list) shouldEqual result
+  }
+
+  "6. permute(List('A,'B','C'))" should "List(List(A, B, ), List(B, A, C), List(B, C, A), " +
+    "List(A, C, B), List(C, A, B), List(C, B, A))" in {
+    val list = List("A", "B", "C")
+    val result = List(List("A", "B", "C"), List("B", "A", "C"), List("B", "C", "A"), List("A", "C", "B"), List("C", "A", "B"), List("C", "B", "A"))
+    ListEx.permute(list) shouldEqual result
+  }
+
+  "7. myLast(List(1,2,3))" should "3" in {
+    val list = List(1, 2, 3)
+    val result = 3
+    ListEx.myLast(list).get shouldEqual result
+  }
+
+  "8. myLast(List())" should "none" in {
+    val list = List()
+    val result = None
+    ListEx.myLast(list) shouldEqual result
+  }
+
+  "9. myLast(Nil)" should "none" in {
+    val list = Nil
+    val result = None
+    ListEx.myLast(list) shouldEqual result
+  }
+
+  "10. myButLast(List(1,2,3))" should "2" in {
+    val list = List(1, 2, 3)
+    val result = 2
+    ListEx.myButLast(list).get shouldEqual result
+  }
+
+  "11. myButLast(List())" should "none" in {
+    val list = List()
+    val result = None
+    ListEx.myButLast(list) shouldEqual result
+  }
+
+  "12. myButLast(Nil)" should "none" in {
+    val list = Nil
+    val result = None
+    ListEx.myButLast(list) shouldEqual result
+  }
+
+  "13. myButLast(List('A'))" should "none" in {
+    val list = List('A')
+    val result = None
+    ListEx.myButLast(list) shouldEqual result
+  }
+
+  "14. myButLast(List('A', 'B'))" should "none" in {
+    val list = List("A", "B")
+    val result = "A"
+    ListEx.myButLast(list).get shouldEqual result
+  }
+
+  "15. penultimoYUltimo(List(1,2,3))" should "List(2,3)" in {
+    val list = List(1, 2, 3)
+    val result = List(2, 3)
+    ListEx.penultimoYUltimo(list) shouldEqual result
+  }
+
+  "16. penultimoYUltimo(List())" should "Nil" in {
+    val list = List()
+    val result = Nil
+    ListEx.penultimoYUltimo(list) shouldEqual result
+  }
+
+  "17. penultimoYUltimo(Nil)" should "none" in {
+    val list = Nil
+    val result = Nil
+    ListEx.penultimoYUltimo(list) shouldEqual result
+  }
+
+  "18. penultimoYUltimo(List('A'))" should "none" in {
+    val list = List('A')
+    val result = Nil
+    ListEx.penultimoYUltimo(list) shouldEqual result
+  }
+
+  "19. penultimoYUltimo(List('A', 'B'))" should "none" in {
+    val list = List("A", "B")
+    val result = List("A", "B")
+    ListEx.penultimoYUltimo(list) shouldEqual result
+  }
+
+  "20. elementAt(List(1,2,3,4,5,6),2)" should "2" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    val result = 2
+    ListEx.elementAt(list, 2) shouldEqual result
+  }
+
+  "21. elementAt(List(1,2,3,4,5,6),0)" should "1" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    val result = 1
+    ListEx.elementAt(list, 0) shouldEqual result
+  }
+
+  "22. elementAt(List(1,2,3,4,5,6),1)" should "1" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    val result = 1
+    ListEx.elementAt(list, 0) shouldEqual result
+  }
+
+  "23. mylenght(List(1,2,3,4,5,6))" should "6" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    val result = 6
+    ListEx.mylenght(list) shouldEqual result
+  }
+
+  "24. mylenght(List())" should "0" in {
+    val list = List()
+    val result = 0
+    ListEx.mylenght(list) shouldEqual result
+  }
+
+  "25. mylenght(List())" should "1" in {
+    val list = List(1)
+    val result = 1
+    ListEx.mylenght(list) shouldEqual result
+  }
+
+
+  "26. mylenghtL(List(1,2,3,4,5,6))" should "6" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    val result = 6
+    ListEx.mylenghtL(list) shouldEqual result
+  }
+
+  "27. mylenghtL(List())" should "0" in {
+    val list = List()
+    val result = 0
+    ListEx.mylenghtL(list) shouldEqual result
+  }
+
+  "28. mylenghtL(List())" should "1" in {
+    val list = List(1)
+    val result = 1
+    ListEx.mylenghtL(list) shouldEqual result
+  }
+
+  "29. mylenghtL(List(List(List(1)))" should "1" in {
+    val list = List(List(List(1)))
+    val result = 1
+    ListEx.mylenghtL(list) shouldEqual result
+  }
+
+  "30. mylenghtR(List(1,2,3,4,5,6))" should "6" in {
+    val list = List(1, 2, 3, 4, 5, 6)
+    val result = 6
+    ListEx.mylenghtR(list) shouldEqual result
+  }
+
+  "31. mylenghtR(List())" should "0" in {
+    val list = List()
+    val result = 0
+    ListEx.mylenghtR(list) shouldEqual result
+  }
+
+  "32. mylenghtR(List())" should "1" in {
+    val list = List(1)
+    val result = 1
+    ListEx.mylenghtR(list) shouldEqual result
+  }
+
+  "33. mylenghtR(List(List(List(1))))" should "1" in {
+    val list = List(List(List(1)))
+    val result = 1
+    ListEx.mylenghtR(list) shouldEqual result
+  }
+
+  "34. isPalindrome(List(1,2,3,4,3,2,1))" should "true" in {
+    val list = List(1, 2, 3, 4, 3, 2, 1)
+    val result = true
+    ListEx.isPalindrome(list) shouldEqual result
+  }
+
+  "35. isPalindrome(List(1,2,3,4,3,2,1,2))" should "false" in {
+    val list = List(1, 2, 3, 4, 3, 2, 1, 2)
+    val result = false
+    ListEx.isPalindrome(list) shouldEqual result
+  }
+
+  "36. isPalindrome(List())" should "false" in {
+    val list = List()
+    val result = true
+    ListEx.isPalindrome(list) shouldEqual result
+  }
+
+  "37. isPalindrome(List(1))" should "true" in {
+    val list = List()
+    val result = true
+    ListEx.isPalindrome(list) shouldEqual result
+  }
+
+  "38. flatten(Const(List(Elem(2), Const(List(Elem(1), Const(List(Elem(3), Elem(4))), Elem(5))))))" should " List(2,1,3,4,5)" in {
+    val list = ListEx.Const(List(ListEx.Elem(2), ListEx.Const(List(ListEx.Elem(1), ListEx.Const(List(ListEx.Elem(3), ListEx.Elem(4))), ListEx.Elem(5)))))
+    val result = List(2, 1, 3, 4, 5)
+    ListEx.flatten(list) shouldEqual result
+  }
+
+  "39. flatten(Const(List(Elem('a'))))" should " List(a)" in {
+    val list = ListEx.Const(List(ListEx.Elem("a")))
+    val result = List("a")
+    ListEx.flatten(list) shouldEqual result
+  }
+
+  "40. compress(List(1,1,1,1,2,3,4,4,4))" should " List(1,2,3,4)" in {
+    val list = List(1, 1, 1, 1, 2, 3, 4, 4, 4)
+    val result = List(1, 2, 3, 4)
+    ListEx.compress(list) shouldEqual result
+  }
+
+  "41. compress(List('a','a','a','b','b','c','c','d','d'))" should " List('a','b','c,'d')" in {
+    val list = List('a', 'a', 'a', 'b', 'b', 'c', 'c', 'd', 'd')
+    val result = List('a', 'b', 'c', 'd')
+    ListEx.compress(list) shouldEqual result
+  }
+
+  "42. pack(List(1,1,1,1,2,3,4,4,4))" should " List(List(1, 1, 1, 1), List(2), List(3), List(4, 4, 4))" in {
+    val list = List(1, 1, 1, 1, 2, 3, 4, 4, 4)
+    val result = List(List(1, 1, 1, 1), List(2), List(3), List(4, 4, 4))
+    ListEx.pack(list) shouldEqual result
+  }
+
+  "43. myrndSelect((List('a', 'b', 'c', 'd', 'e', 'f'))" should " rndm Elements selected on a list" in {
+    val list = List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+    val result = "A"
+    ListEx.myrndSelect(list, 4) shouldEqual result
+  }
+
+  "44. mydiffSelect(List( 1 , 1 , 1 , 1 , 2 , 3 , 4 , 4 , 4 ))" should " List of random elements between 0 to 49" in {
+    val result = "A"
+    ListEx.mydiffSelect(6, 49) shouldEqual result
+  }
+
+  "45. mypermuteSelect(List('a', 'b', 'c', 'd', 'e', 'f')" should " A permutation of a list " in {
+    val list = List('a', 'b', 'c', 'd', 'e', 'f')
+    val result = "A"
+    ListEx.myRndmPermute(list) shouldEqual result
+  }
+
+  "46. mypermuteSelect2(List('a', 'b', 'c', 'd', 'e', 'f')" should " A permutation of a list " in {
+    val list = List('a', 'b', 'c', 'd', 'e', 'f')
+    val result = "A"
+    ListEx.myRndmPermute2(list) shouldEqual result
+  }
+
+  "47. myCombinations(List('a', 'b', 'c', 'd', 'e', 'f'))" should " List(1,2,3,4)" in {
+    val list = List('a', 'b', 'c', 'd', 'e', 'f')
+    val result = "A"
+    ListEx.mycombinations(list, 3) shouldEqual result
+  }
+
+  "48. encode(List(1,1,1,1,2,3,4,4,4))" should "  List((4,1), (1,2), (1,3), (3,4))" in {
+    val list = List(1, 1, 1, 1, 2, 3, 4, 4, 4)
+    val result = List((4,1), (1,2), (1,3), (3,4))
+    ListEx.encode(list) shouldEqual result
+  }
+
+  "49. encode(List('a', 'a', 'c', 'c', 'c', 'f','k', 'k','k','k'))" should " List((2,'a'), (3,'c'), (1,'f'), (4,'k')) " in {
+    val list = List('a', 'a', 'c', 'c', 'c', 'f','k', 'k','k','k')
+    val result = List((2,'a'), (3,'c'), (1,'f'), (4,'k'))
+    ListEx.encode(list) shouldEqual result
+  }
+
+  "50. encodeModified(List('a', 'a', 'c', 'c', 'c', 'f','k', 'k','k','k'))" should " List((2,a), (3,c), 'f', (4,k))" in {
+    val list = List('a', 'a', 'c', 'c', 'c', 'f','k', 'k','k','k')
+    val result = List((2,'a'), (3,'c'), (1,'f'), (4,'k'))
+    ListEx.encodeModified(list) shouldEqual result
+  }
+
+  "51. encodeModified(List(1, 1, 1, 1, 2, 3, 4, 4, 4))" should " List((4,1), 2, 3, (3,4))" in {
+    val list = List(1, 1, 1, 1, 2, 3, 4, 4, 4)
+    val result = List((4,1), 2, 3, (3,4))
+    ListEx.encodeModified(list) shouldEqual result
+  }
+
+  "52. decodeModified(List('a', 'a', 'c', 'c', 'c', 'f','k', 'k','k','k'))" should " List((2,a), (3,c), 'f', (4,k))" in {
+    val list = List((4,1), 2, 3, (3,4))
+    val result = List((4,1), 2, 3, (3,4))
+    ListEx.decodeModified(list) shouldEqual result
+  }
+
+
 }
